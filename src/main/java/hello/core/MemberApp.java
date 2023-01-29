f0e3f27619bb0f09364037b724bd721b74028166
hc84.shin <dzzwe@naver.com>
2023-01-29 22:51:04 +0900

    스프링 컨테이너 적용

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -1,12 +1,18 @@
 package hello.core;
 
 import hello.core.member.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
 //        MemberService memberService = new MemberServiceImpl(new MemoryMemberRepository());
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class); //AppConfig의 환경설정 호출
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);//bean
+
         Member memberA = new Member(1L, "memberA", Grade.VIP);
         memberService.join(memberA);
 
