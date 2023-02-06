f1ef8e4dd5b8d31a3eec546bffbd62079acbae8a
hc84.shin <dzzwe@naver.com>
2023-02-06 22:05:12 +0900

    빈 초기화와 소멸 메서드

---
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,41 @@
+package hello.core.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
+
+    private String url;
+
+    public NetworkClient() {
+
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void connect() {
+        System.out.println("connect : " + url);
+    }
+
+    public void disconnect() {
+        System.out.println("disconnect : " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call : " + url + " message : " + message);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
+    }
+}
+
